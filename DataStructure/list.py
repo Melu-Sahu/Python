ca05e409975c78f2bb9d97951c3ed6c97a7281d2
Melu-Sahu <melusahu.dev@gmail.com>
2026-05-06 01:08:05 +0530

    List Completed

---
+++ DataStructure/list.py
@@ -0,0 +1,69 @@
+
+# List In Python 
+
+# creating list
+a = [1, 2,3,4,5,6]  #list of integers
+b = ['apple', 'banana', 'mango'] # list of strings
+c = [1, 'cherry', 12.05, True] # Hetrogeneous List (Mixed Data Types)
+
+# creating list using list custructor
+
+li = list((1, "Hii", 12.5, False))
+name = list("Melu")
+print(a)
+print(b)
+print(c)
+print(li)
+print(name)
+
+a = [2]*3
+print(a)
+
+
+# Accessing List Elements
+
+print(b[0])
+print(b[1])
+print(b[-1])
+print(b[0:2])
+
+
+# #############  Adding Elements into List
+
+b.append("cherry")  # append cherry at last index
+print(b)
+
+b.insert(2, "Pineapple")  # add in 3rd place (2  index)
+print(b)
+
+b.extend(["Greps", "Water Mellon", "Kiwi"])  # add multiple items at once in last index
+print(b)
+
+
+
+########## Removing Elements from list
+
+b.remove("banana")   # will remove the banana from the list
+print(b)
+
+popped_val = b.pop(1) # will remove the element from list present at 1 index and will return the value of that
+print(popped_val)
+print(b)
+
+del b[0]   # will delete the 0th element of the list
+print(b)
+
+# b.clear()  # will remove all the elements from the list
+# print(b)
+
+######################### Iterating over the list
+
+for fruit in b:
+    print(fruit)
+
+
+###################### List Comprehension
+
+squares = [x**2 for x in range(1,6)]
+print(squares)
+
